9e0c0dc1bdb9ac02979c94cbfc95f9e318a00d1e
muhammad <muhammadnuraliyev119@gmail.com>
2024-10-14 14:13:39 +0500

    muhammad

---
+++ script.js
@@ -0,0 +1,111 @@
+// 1-misol
+
+
+let names=prompt("ismingizni kiriting");
+let familya=prompt("familiyangizni kiriting");
+let age=+prompt("yoshingizni kiriting");
+let city=prompt("manzilingizni kiriting");
+let kasb=prompt("kasbingizni kiriting");
+let otasi=prompt("otangizning ismini kiriting");
+let otasining_yoshi=+prompt("otangizning yoshini kiriting");
+let otasining_manzili=prompt("otangizning manzilini kiriting");
+let otasining_kasbi=prompt("otangizning kasbini kiriting");
+let otasining_otasi=prompt("otangizning otasini kiriting");
+
+console.log(`
+ismi: ${names}
+familiyasi: ${familya}
+yoshi: ${age}
+manzili: ${city}
+kasbi: ${kasb}
+otasining ismi: ${otasi}
+otasining yoshi: ${otasining_yoshi}
+otasining manzili: ${otasining_manzili}
+otasining kasbi: ${otasining_kasbi}
+otasining otasini: ${otasining_otasi}
+`);
+
+alert(`
+
+ismi: ${names}
+familiyasi: ${familya}
+yoshi: ${age}
+manzili: ${city}
+kasbi: ${kasb}
+otasining ismi: ${otasi}
+otasining yoshi: ${otasining_yoshi}
+otasining manzili: ${otasining_manzili}
+otasining kasbi: ${otasining_kasbi}
+otasining otasini: ${otasining_otasi}
+`)
+
+// 1-misol tugadi
+
+
+
+// 2-misol
+
+
+
+// let olma = +prompt("olma narxini kiriting");
+// let banan = +prompt("banan narxini kiriting");
+// let qovun = +prompt("qovun narxini kiriting");
+// let uzum = +prompt("uzum narxini kiriting");
+// let gilos = +prompt("gilos narxini kiriting");
+// let narx = olma + banan + qovun + uzum + gilos;
+// console.log(narx);
+// alert("jami narxlari:" + narx);
+
+
+// 2-misol tugadi
+
+
+// 3-misol
+
+
+// let matematika = +prompt("matematikadan olgan balingizni kiriting");
+// let rus = +prompt("rus tilidan dan olgan balingizni  kiriting");
+// let fransuz = +prompt("fransuz tilidan olgan balingizni  kiriting");
+// let kimyo = +prompt("kimyo fanidan olgan balingizni kiriting");
+// let biolog = +prompt("biologiya fanidan olgan balingizni kiriting");
+// let jami = matematika + rus + fransuz + kimyo + biolog;
+// console.log(jami);  
+// alert("o'rtacha balingiz:" + jami /5);
+
+
+// 3-misol tugadi
+
+
+
+// Qo'shimcha
+
+// let bank = +prompt("bankingizdagi balansingizni kiriting");
+// let balans = +prompt("qancha pul kiritmoqchisiz");
+// let jami = bank + balans;
+// console.log(jami);
+// alert("jami balansingiz:" + jami);
+
+
+// qo'shimcha tugadi
+
+
+
+
+// qo'shimcha
+
+
+
+// let yosh = +prompt("Yoshingizni kiriting");
+// let natija = yosh;
+
+// if (yosh >= 20) {
+//   natija = "Siz kredit olishingiz mumkin.";
+// } else {
+//   natija = "Afsuski, siz hali 20 yoshga to‘lmagansiz, kredit ololmaysiz.";
+// }
+
+// alert(natija);
+
+
+
+// qo'shimcha tugadi
